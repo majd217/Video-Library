9b066d7dce567e132ffc4a5dea10c23848ea0b45
majd217 <m.amireh@gju.edu.jo>
2023-05-05 19:25:02 +0300

    js rdeisgned

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 
-const submenu = document.getElementById("submenu");
-const icon = document.getElementById("tb");
+const iconArray = document.getElementsByClassName('animated-icon'); //returns array
+const subMenuArray = document.getElementsByClassName('item-content');
 
-icon.addEventListener('hover',() => { 
-    submenu.style.display = "block";} );
+for(let icon of iconArray){
+    icon.addEventListener('click',(event) =>
+        { 
+            for(let subMenu of subMenuArray)
+            {
+               if(subMenu != event.target.parentElement.nextElementSibling)
+                subMenu.style.display = 'none';
+            }
+            event.target.parentElement.nextElementSibling.style.display = (event.target.parentElement.nextElementSibling.style.display == 'none') ? 'flex' : "none";
+            console.log(event.target.parentElement.nextElementSibling);
+       });
 
-
+}
