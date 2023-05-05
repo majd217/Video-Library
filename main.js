b2829422008718a278b48254f1acff4fd51c5f15
majd217 <m.amireh@gju.edu.jo>
2023-05-05 16:42:14 +0300

    js added

---
+++ main.js
@@ -0,0 +1,8 @@
+
+const submenu = document.getElementById("submenu");
+const icon = document.getElementById("tb");
+
+icon.addEventListener('hover',() => { 
+    submenu.style.display = "block";} );
+
+
